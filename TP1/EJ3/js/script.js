6489e8a4504a58c4ec2a6c4e66e19d1df08459e1
PabloCalandria <54867932+PabloCalandria@users.noreply.github.com>
2020-08-29 11:12:40 -0300

    Se modifico el ejercicio3

--- TP1/EJ3/js/script.js
+++ TP1/EJ3/js/script.js
@@ -1,21 +1,19 @@
-var ctx = document.getElementById("myCanvas").getContext("2d");
+let ctx = document.querySelector("#myCanvas").getContext("2d");
 
-var width = 150;
-var height = 150;
-var imageData = ctx.createImageData(width, height);
+let width = 350;
+let height = 350;
+let imageData = ctx.createImageData(width, height);
 
-for (var x = 0; x < width; x++) {
-    for (var y = 0; y < height; y++) {
+for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
         setPixel(imageData, x, y, 255, 128, 0, 255);
     }
 }
 
 ctx.putImageData(imageData, 0, 0);
 
-//////////////////////////////////////////////////
-
 function setPixel(imageData, x, y, r, g, b, a) {
-    index = (x + y * imageData.width) * 4;
+    index = (x + y * imageData.width) * 4; //el 4 son el R-G-B-A (RedGreenBlueAlpha)
     imageData.data[index + 0] = r;
     imageData.data[index + 1] = g;
     imageData.data[index + 2] = b;
