c85e59cb3ee6e0e5bca63330f763db2d43bc6fa8
PabloCalandria <54867932+PabloCalandria@users.noreply.github.com>
2020-09-18 17:41:34 -0300

    Se comenzo el entregable 2

--- TP2/Ej1-2/js/Main.js
+++ TP2/Ej1-2/js/Main.js
@@ -25,16 +25,16 @@ function drawFigures() {
 function addRectangle() {
     let posX = Math.round(Math.random() * canvasWidth);
     let posY = Math.round(Math.random() * canvasHeight);
-    let color = randomRGBA();
-    let rect = new Rect(posX, posY, 20, 20, color, context);
+    let color = getOpcionColor(posX, posY);
+    let rect = new Rect(posX, posY, 100, 100, color, context);
     figures.push(rect);
 }
 
 function addCircle() {
     let posX = Math.round(Math.random() * canvasWidth);
     let posY = Math.round(Math.random() * canvasHeight);
-    let color = randomRGBA();
-    let circle = new Circle(posX, posY, 10, color, context);
+    let color = getOpcionColor(posX, posY);
+    let circle = new Circle(posX, posY, 50, color, context);
     figures.push(circle);
 }
 
@@ -42,7 +42,7 @@ function addCircle() {
 // Evento temporal para agregar figuras
 function addFigures() {
     addFigure();
-    if (figures.length < 30) {
+    if (figures.length < 10) {
         setTimeout(addFigures, 333);
     }
 }
@@ -58,6 +58,30 @@ function randomRGBA() {
     let b = Math.round(Math.random() * 255);
     let a = 255;
     return `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+function getOpcionColor(posX, posY){
+    let opcion = Math.round(Math.random() * 2);
+    if(opcion == 0){
+        return randomRGBA();
+    }
+    if(opcion == 1){
+        let gradiente = context.createLinearGradient(0,0,800,0);
+        gradiente.addColorStop(0,randomRGBA());
+        gradiente.addColorStop(1,randomRGBA());
+        return gradiente;
+    }
+    if(opcion == 2){
+        let img = new Image();
+        img.src = "./images/ficha.jpg";
+        img.onload = function(){
+            img.width = 100;
+            img.height = 100;
+            let imagen = context.createPattern(img, "repeat");
+            return imagen;
+        }
+    }
+    
 }
 
 function clearCanvas() {
